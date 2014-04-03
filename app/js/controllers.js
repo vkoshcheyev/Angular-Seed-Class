2fa20cdea9cd2ac8da9dabc71f51972410963d8c
vkoshcheyev <koshcheyev_vladimir@hotmail.com>
2014-04-03 11:57:49 -0500

    modified parcial1 and parcial2

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,30 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', [ '$scope', function($scope) {
+     $scope.car = {make: 'Honda',
+                   model: 'Civic',
+                   year: 2014,
+                   price: 24000  
+                  }       
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', ['$scope',function($scope) {
+     $scope.cars = [
+	                {make: 'Honda',
+                     model: 'Civic',
+                     year: 2014,
+                     price: 24000  
+                    },
+	                {make: 'Honda',
+                     model: 'Odyssey',
+                     year: 2011,
+                     price: 22100  
+                    },
+	                {make: 'Toyota',
+                     model: 'Sienna',
+                     year: 2014,
+                     price: 26100  
+                    },
+					]
 
   }]);
